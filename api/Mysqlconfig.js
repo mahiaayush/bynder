508d2897d14be6fe5a74e42e094337d1a3c36781
Ajeet.Mishra <ajeet.mishra@Nod-Mac-343.local>
2019-05-29 17:54:56 +0530

    mysql date base config changes

--- api/Mysqlconfig.js
+++ api/Mysqlconfig.js
@@ -1,8 +1,15 @@
-let config = {
+// let config = {
+//     host    : 'localhost',
+//     user    : 'root',
+//     password: 'www@1234',
+//     database: 'bynder_data'
+//   };
+
+  let config = {
     host    : 'localhost',
-    user    : 'root',
-    password: 'www@1234',
-    database: 'bynder_data'
+    user    : 'bynder',
+    password: 'W@rdint22$',
+    database: 'bynder_reporting_data'
   };
    
   module.exports = config;
